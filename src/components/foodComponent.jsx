ca4d22029c81dbec37af10c3933cc54160fcbf01
Tanupat Sittiwong <Tanupat_s@kkumail.com>
2025-05-13 00:05:35 +0700

    add stock

--- src/components/foodComponent.jsx
+++ src/components/foodComponent.jsx
@@ -3,23 +3,24 @@ import { Card, Row, Col, Image } from "react-bootstrap"
 
 export default function FoodComponent({ data }) {
 
-    const { foodname, Price, code, img } = data;
+    const { foodname, Price, code, img, status } = data;
     useEffect(() => {
 
     }, []
     )
     return <>
 
-        <Card style={{ padding: '0', marginBottom: '12px',cursor:'pointer' }} key={code}>
+        <Card style={{ padding: '0', marginBottom: '12px', cursor: 'pointer' }} key={code}>
 
             <Card.Body style={{ padding: '0px' }}>
                 <Row>
                     <Col md={5}>
-                        <Image style={{ width: '100%', height: '100px', objectFit: 'cover',borderRadius:8 }}
+                        <Image style={{ width: '100%', height: '100px', objectFit: 'cover', borderRadius: 8 }}
                             src={import.meta.env.VITE_API_URL + '/images/' + img} />
                     </Col>
                     <Col md={7} >
                         <div className="text-left p-2">
+                            { status===1 ? "" :<p style={{color:"red"}}> ของหมด </p>  }
                             <h6>{foodname} <br /> ฿{Price} </h6>
                         </div>
                     </Col>
