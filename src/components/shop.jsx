dcd2c4e770a8764696f450ad60f70d0f60cc47b4
Tanupats <Tanupat_s@kkumail.com>
2026-01-22 16:57:33 +0700

    edit app

--- src/components/shop.jsx
+++ src/components/shop.jsx
@@ -9,7 +9,7 @@ const ShopData = () => {
     const { userid, name } = useParams();
     const router = useNavigate();
     const [data, setData] = useState([]);
-   
+
     const getShopData = async () => {
         await axios.get(`${import.meta.env.VITE_API_URL}/shop`)
             .then(res => { setData(res.data); });
@@ -21,7 +21,7 @@ const ShopData = () => {
             localStorage.setItem("name", name);
             getShopData();
         }
-       
+
     }, [])
 
     return (<>
@@ -29,59 +29,59 @@ const ShopData = () => {
             <Card.Body>
                 <Row>
                     {
-                      data.map((item, index) => {
+                        data.map((item, index) => {
 
-                         if(item.id === 1) return (
-                                    <React.Fragment key={index}>
-                                        <Col
-                                            style={{ cursor: 'pointer' }}
-                                            md={4}
-                                            xs={12}
-                                            className="mt-2"
+                            if (item.id === 1) return (
+                                <React.Fragment key={index}>
+                                    <Col
+                                        style={{ cursor: 'pointer' }}
+                                        md={4}
+                                        xs={12}
+                                        className="mt-2"
 
-                                        >
-                                            <Card>
-                                                <Card.Body style={{ padding: 10 }}>
+                                    >
+                                        <Card>
+                                            <Card.Body style={{ padding: 10 }}>
 
-                                                    <Card.Title>{item.name}</Card.Title>
-                                                    <div className="mb-2">
+                                                <Card.Title>{item.name}</Card.Title>
+                                                <div className="mb-2">
 
-                                                        <h4>
-                                                            {
-                                                                item.is_open ? (<Badge bg="success" style={{ fontWeight: 500 }}>
-                                                                    เปิด <AccessTimeIcon />
-                                                                    {item.open_time}
-                                                                </Badge>) : (<>
+                                                    <h4>
+                                                        {
+                                                            item.is_open ? (<Badge bg="success" style={{ fontWeight: 500 }}>
+                                                                เปิด <AccessTimeIcon />
+                                                                {item.open_time}
+                                                            </Badge>) : (<>
 
 
-                                                                    <Badge bg="danger">{item.open_time}  </Badge>
-                                                                </>)
-                                                            }
-                                                        </h4>
-                                                    </div>
+                                                                <Badge bg="danger">{item.open_time}  </Badge>
+                                                            </>)
+                                                        }
+                                                    </h4>
+                                                </div>
 
-                                                    <Image
-                                                        style={{
-                                                            width: "100%",
-                                                            height: "200px",
-                                                            objectFit: "cover",
-                                                            cursor: item.is_open ? "pointer" : "not-allowed",
-                                                            opacity: item.is_open ? 1 : 0.5,
-                                                        }}
-                                                        src={`${import.meta.env.VITE_API_URL}/images/${item.photo}`}
-                                                        onClick={() => {
-                                                            if (item.is_open) {
-                                                                router(`/foodmenu/${item.shop_id}`);
-                                                            }
-                                                        }}
-                                                    />
+                                                <Image
+                                                    style={{
+                                                        width: "100%",
+                                                        height: "200px",
+                                                        objectFit: "cover",
+                                                        cursor: item.is_open ? "pointer" : "not-allowed",
+                                                        opacity: item.is_open ? 1 : 0.5,
+                                                    }}
+                                                    src={`${import.meta.env.VITE_API_URL}/images/${item.photo}`}
+                                                    onClick={() => {
+                                                        if (item.is_open) {
+                                                            router(`/foodmenu/${item.shop_id}`);
+                                                        }
+                                                    }}
+                                                />
 
-                                                </Card.Body>
-                                            </Card>
-                                        </Col>
-                                    </React.Fragment>
-                                );
-                            })
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                </React.Fragment>
+                            );
+                        })
                     }
 
                 </Row>
